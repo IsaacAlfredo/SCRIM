d1d86d92ee06e6b35bf80b4b25a780c3d0e5befc
Isaac Alfredo <isaacfreitas23@gmail.com>
2022-06-29 17:15:10 -0300

    profile route fix

--- scrim/src/components/profileInfo/index.jsx
+++ scrim/src/components/profileInfo/index.jsx
@@ -3,31 +3,26 @@ export function ProfileInfo(props) {
     <section className="ProfileInfo">
       <img src="" alt="Foto de perfil" />
       <strong>Nome de Usuário</strong>
-      <div className="gameProfile">
-        <strong>Conta main:</strong>
-        <small>nick da conta</small>
-        <strong>Elo:</strong>
-        <small>elo da conta</small>
-        <strong>Op.gg:</strong>
-        <small>link opgg</small>
-      </div>
+
       <table>
-        <tr>
-          <td>Conta Main:</td>
-          <td>{props.nick}</td>
-        </tr>
-        <tr>
-          <td>Elo:</td>
-          <td>{props.elo}</td>
-        </tr>
-        <tr>
-          <td>Op.gg</td>
-          <td>{props.opgg} </td>
-        </tr>
-        <tr>
-          <td>Smurfs:</td>
-          <td>{props.smurfsNicks}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>Conta Main:</td>
+            <td>{props.nick}</td>
+          </tr>
+          <tr>
+            <td>Elo:</td>
+            <td>{props.elo}</td>
+          </tr>
+          <tr>
+            <td>Op.gg</td>
+            <td>{props.opgg} </td>
+          </tr>
+          <tr>
+            <td>Smurfs:</td>
+            <td>{props.smurfsNicks}</td>
+          </tr>
+        </tbody>
       </table>
     </section>
   );
--- scrim/src/pages/profile/index.jsx
+++ scrim/src/pages/profile/index.jsx
@@ -1,15 +1,22 @@
 import { ProfileInfo } from "../../components/profileInfo";
 
 export function Profile() {
-  <div className="Profile">
-    <header>
-      <h1>SCRIM</h1>
-      <nav>
-        <a href="">Buscar scrims</a>
-        <a href="">Meu time</a>
-        <a href="">Editar perfil</a>
-      </nav>
-    </header>
-    <ProfileInfo />
-  </div>;
+  return (
+    <div className="Profile">
+      <header>
+        <h1>SCRIM</h1>
+        <nav>
+          <a href="">Buscar scrims</a>
+          <a href="">Meu time</a>
+          <a href="">Editar perfil</a>
+        </nav>
+      </header>
+      <ProfileInfo
+        nick="Shiota"
+        elo="Platina"
+        opgg="br.op.gg"
+        smurfsNicks="Kayano Kaede"
+      />
+    </div>
+  );
 }
