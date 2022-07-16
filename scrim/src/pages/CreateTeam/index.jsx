47f0ec96f7096726a23375cdd3e59b9209145406
Isaac Alfredo <isaacfreitas23@gmail.com>
2022-07-15 23:17:31 -0300

    home css

--- scrim/src/pages/CreateTeam/index.jsx
+++ scrim/src/pages/CreateTeam/index.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { Header } from "../../components/Header";
+import "./style.css";
 
 export function CreateTeam() {
   return (
--- scrim/src/pages/home/index.jsx
+++ scrim/src/pages/home/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Login } from "../../components/login";
+import "./style.css";
 
 function Home() {
   return (
